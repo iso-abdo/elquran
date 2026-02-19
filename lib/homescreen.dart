7f32003dcb3274c0b5b56791bc8c415caa298480
iso <isoemo79@gmail.com>
2026-02-19 02:56:49 +0000

    Revert "add theme in main.dart"
    This reverts commit 6f45eb7f911b3cfd75186724e1e92e91913612e9.

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -14,12 +14,9 @@ class HomeScreen extends StatelessWidget {
         ),
         Scaffold(
           appBar: AppBar(
-            title: Text('quran', style: Theme
-                .of(context)
-                .textTheme
-                .bodyLarge),
+            title: Text('quran', style: Theme.of(context).textTheme.bodyLarge),
           ),
-        )
+        ))
 
       ],
     );
